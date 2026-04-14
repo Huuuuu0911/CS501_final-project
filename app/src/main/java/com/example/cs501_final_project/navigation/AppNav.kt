db478c99a12a362e7262406c5f7499559ad1ad47
Jefferywan <114625429+Jefferywan@users.noreply.github.com>
2026-04-14 17:54:11 -0400

    previous wrong move

--- app/src/main/java/com/example/cs501_final_project/navigation/AppNav.kt
+++ app/src/main/java/com/example/cs501_final_project/navigation/AppNav.kt
@@ -151,7 +151,13 @@ fun AppNav() {
                 )
             }
 
-
+            composable(
+                route = "follow_up/{part}/{symptomText}/{painLevel}",
+                arguments = listOf(
+                    navArgument("part") { type = NavType.StringType },
+                    navArgument("symptomText") { type = NavType.StringType },
+                    navArgument("painLevel") { type = NavType.IntType }
+                )
             ) { backStackEntry ->
                 val part = backStackEntry.arguments?.getString("part") ?: ""
                 val symptomText = backStackEntry.arguments?.getString("symptomText") ?: ""
