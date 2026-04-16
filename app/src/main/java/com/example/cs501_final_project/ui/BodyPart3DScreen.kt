53b0aec7af64beb01f9910a9e2f36de68ab4dcde
Jefferywan <114625429+Jefferywan@users.noreply.github.com>
2026-04-15 21:44:19 -0400

    change 3D

--- app/src/main/java/com/example/cs501_final_project/ui/BodyPart3DScreen.kt
+++ app/src/main/java/com/example/cs501_final_project/ui/BodyPart3DScreen.kt
@@ -1906,8 +1906,8 @@ private fun getBodyOverlaySpec(side: String, useTwoPane: Boolean): BodyOverlaySp
 
     val hotspots = when (side) {
         "Front" -> listOf(
-            BodyHotspot("Forehead", 0.50f, 0.08f, 0.052f),
-            BodyHotspot("Face", 0.50f, 0.10f, 0.052f),
+            BodyHotspot("Forehead", 0.50f, 0.0001f, 0.052f),
+
             BodyHotspot("Neck", 0.50f, 0.17f, 0.050f),
 
             BodyHotspot("Left Shoulder", 0.30f, 0.15f, 0.052f),
@@ -1917,22 +1917,22 @@ private fun getBodyOverlaySpec(side: String, useTwoPane: Boolean): BodyOverlaySp
             BodyHotspot("Center Chest", 0.50f, 0.34f, 0.052f),
             BodyHotspot("Right Chest", 0.57f, 0.33f, 0.052f),
 
-            BodyHotspot("Upper Abdomen", 0.50f, 0.46f, 0.052f),
-            BodyHotspot("Lower Abdomen", 0.50f, 0.56f, 0.052f),
+            BodyHotspot(" Abdomen", 0.50f, 0.2f, 0.052f),
+
 
             BodyHotspot("Left Arm", 0.06f, 0.15f, 0.050f),
             BodyHotspot("Right Arm", 0.94f, 0.15f, 0.050f),
 
-            BodyHotspot("Pelvis", 0.50f, 0.67f, 0.052f),
+
 
             BodyHotspot("Left Thigh", 0.38f, 0.5f, 0.048f),
             BodyHotspot("Right Thigh", 0.60f, 0.5f, 0.048f),
 
-            BodyHotspot("Left Knee", 0.46f, 0.90f, 0.046f),
-            BodyHotspot("Right Knee", 0.54f, 0.90f, 0.046f),
+            BodyHotspot("Left Knee", 0.40f, 0.65f, 0.046f),
+            BodyHotspot("Right Knee", 0.60f, 0.65f, 0.046f),
 
-            BodyHotspot("Left Shin", 0.46f, 0.98f, 0.044f),
-            BodyHotspot("Right Shin", 0.54f, 0.98f, 0.044f)
+            BodyHotspot("Left Shin", 0.40f, 0.80f, 0.044f),
+            BodyHotspot("Right Shin", 0.60f, 0.80f, 0.044f)
         )
 
         "Back" -> listOf(
